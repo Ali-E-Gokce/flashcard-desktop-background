94a2152abf442daacad41c45e7798034760bfd35
Ali-E-Gokce <a.erengoekcelioglu@gmail.com>
2019-07-01 15:22:44 +0300

    added sample sentence

--- flashcard_background_setter.py
+++ flashcard_background_setter.py
@@ -6,18 +6,19 @@ import subprocess
 from time import sleep
 
 #where the generated picture will be saved
-OUT_PATH = "/Users/alierengokcelioglu/desktop/try_stuff/background.jpeg"
-#how much empty distance to leave on the right side of the screen
+OUT_PATH = "/Users/alierengokcelioglu/desktop/personal_coding/flashcard-desktop-background/background.jpeg"
+
+#to leave empty space on the right side of the screen
 #depends on how full your desktop is.
 #You might want to keep it low if you have very long input sentences
-RIGHT_GAP = 800
+RIGHT_GAP = 1600
 #name of the file where the vocabulary is saved
-INPUT_FILE = "vocab.csv"
+INPUT_PATH = "vocab.csv"
 #you can find other fonds online
 WORD_FONT = ImageFont.truetype('Roboto-LightItalic.ttf', 48)
 TEXT_FONT = ImageFont.truetype('Roboto-Light.ttf', 48)
-#the background is changed every 4 hours
-BACKGROUND_SWAP_INTERVAL = 4*60*60
+#the background is changed every 20 minutes
+BACKGROUND_SWAP_INTERVAL = 20*60
 #this is the script that changes the desktop background
 #you should replace this with a script that works on your machine
 SCRIPT = """/usr/bin/osascript<<END
@@ -26,6 +27,7 @@ set desktop picture to POSIX file "{}"
 end tell
 END
 killall Dock"""
+
 
 #this won't work if there is more than one monitor attached
 #but is the only way I could think of that does not require
@@ -37,12 +39,15 @@ def get_screen_size():
     return width, heigth
 
 #gets a random word and meaning from your list
-def get_vocab(INPUT_FILE):
-    df = pd.read_csv(INPUT_FILE)
-    vocab_pair = df.sample(n = 1)
-    word = vocab_pair["Word"].to_string(index = False)
-    meaning = vocab_pair["Meaning"].to_string(index = False)
-    return word, meaning
+def get_vocab(INPUT_PATHPATHPATH):
+    df = pd.read_csv(INPUT_PATH)
+    pd.options.display.max_colwidth = 5000
+    vocab = df.sample(n = 1)
+    word = vocab["Word"].to_string(index = False)
+    meaning = vocab["Meaning"].to_string(index = False, na_rep = "")
+    context = vocab["Context"].to_string(index = False, na_rep = "")
+
+    return word, meaning, context
 
 
 
@@ -77,10 +82,11 @@ def split_sentence(text):
 #makes the background picture
 def draw():
     width, height = get_screen_size()
-    word, meaning = get_vocab(INPUT_FILE)
-    lines = split_sentence(meaning)
-    text_height = TEXT_FONT.getsize(meaning)[1]
+    word, meaning, context = get_vocab(INPUT_PATH)
+    definition_list = split_sentence(meaning)
+    context_list = split_sentence(context)
     text_color, background_color =  get_color()
+    text_height = TEXT_FONT.getsize(meaning)[1]
     im = Image.new("RGB", (width, height), background_color)
     d = ImageDraw.Draw(im)
     black = (0, 0, 0)
@@ -88,19 +94,20 @@ def draw():
     x_offset = 10
     d.text((x_offset, y_offset), word, fill = text_color, font = WORD_FONT)
     y_offset += 100
-    for line in lines:
+    for line in definition_list:
         y_offset += text_height
         d.text((x_offset, y_offset), line, fill = text_color, font = TEXT_FONT)
-    im.save('{}'.format(OUT_PATH), optimize = True)
-
-
-
+    y_offset += 100
+    text_height = TEXT_FONT.getsize(context)[1]
+    for line in context_list:
+        y_offset += text_height
+        d.text((x_offset, y_offset), line, fill = text_color, font = TEXT_FONT)
+    im.save("{}".format(OUT_PATH), optimize = True)
 
 def new_desktop_background():
     draw()
     subprocess.Popen(SCRIPT.format(OUT_PATH), shell=True)
     #subprocess.Popen("killall Dock", shell=True)
-
 
 if __name__ == "__main__":
     while True:
